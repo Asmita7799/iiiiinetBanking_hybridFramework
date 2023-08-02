e79d4058b5c9668bdb965a24e8959e382891eec5
atulj <atulj@Ganganayak>
2023-08-02 11:07:20 +0530

    Update Esit Customer

--- com.iiiiinetbanking/src/test/java/com/iiiiinetbanking/pageobject/EditCustomerPage.java
+++ com.iiiiinetbanking/src/test/java/com/iiiiinetbanking/pageobject/EditCustomerPage.java
@@ -1,13 +1,25 @@
 package com.iiiiinetbanking.pageobject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class EditCustomerPage {
 
 	
 	WebDriver driver;
+	
+	@FindBy(name="XYZ")
+	WebElement customerDetails;
+	
 	public EditCustomerPage(WebDriver driver)
 	{
 		this.driver = driver;
+	}
+	
+	
+	public void editCustomerDetails()
+	{
+		
 	}
 }
